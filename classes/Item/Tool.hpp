a7c9263660c98342a5896a37f508ec5429c1095f
rozanfa <rozanfadhilalhafdiz@gmail.com>
2022-03-20 19:27:50 +0700

    remove `material` attribute in Item classes

--- classes/Item/Tool.hpp
+++ classes/Item/Tool.hpp
@@ -5,17 +5,15 @@
 
 class Tool : public Item {
 protected:
-    string material;
     int durability;
 public:
-    Tool() : Item(), durability(-1), material("none") {}
-    Tool(int _id, string _name, string _material) : Item(_id, _name, true), durability(10), material(_material) {}
-    Tool(const Tool& other) : Item(other) { material = other.material; durability = other.durability; }
+    Tool() : Item(), durability(-1) {}
+    Tool(int _id, string _name, string _material) : Item(_id, _name, true), durability(10) {}
+    Tool(const Tool& other) : Item(other) { durability = other.durability; }
     void use() { durability--; }; 
 
     void describe() const {
         Item::describe();
-        cout << "Material : " << material << endl;
         cout << "Durability : " << durability << endl;
     }
 
--- classes/Item/TypedNonTool.hpp
+++ classes/Item/TypedNonTool.hpp
@@ -6,16 +6,14 @@
 class TypedNonTool : public NonTool {
 protected:
     string type;
-    string material;
 public:
     TypedNonTool() : NonTool(0, "none"), type("none") {}
-    TypedNonTool(int _id, string _name, string _type, string _material) : NonTool(_id, _name), type(_type), material(_material) {}
+    TypedNonTool(int _id, string _name, string _type) : NonTool(_id, _name), type(_type) {}
     TypedNonTool(const TypedNonTool& other) : NonTool(other) { type = other.type; }
     string getType() const { return this->type == "LOG" ? name : type; }
     void describe() const {
         Item::describe();
         cout << "Type : " << type << endl;
-        cout << "Material : " << material << endl;
     }
 };
 
