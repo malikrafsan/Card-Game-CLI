123a971b6605f093637278621fc6b675736c0eea
Ahmad Romy <ahmadromyzahran@gmail.com>
2022-03-25 21:55:41 +0700

    fix: nerima enter

--- classes/TerminalManager/TerminalManager.cpp
+++ classes/TerminalManager/TerminalManager.cpp
@@ -5,14 +5,16 @@
 TerminalManager::TerminalManager() {;}
 
 vector<string> TerminalManager::readCommand() {
-    cout <<"INSERT COMMAND: ";
     vector<string> ret;
-    string line;
-    getline(cin, line);
-    stringstream ss(line);
-    string word;
-    while(ss >> word) {
-        ret.push_back(word);
+    while((int)ret.size()==0) {
+        cout <<"INSERT COMMAND: ";
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        string word;
+        while(ss >> word) {
+            ret.push_back(word);
+        }
     }
     return ret;
 }
