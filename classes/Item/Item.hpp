4b309d20a26965bcc55337aef98ef74417eb0919
rozanfa <rozanfadhilalhafdiz@gmail.com>
2022-03-25 22:41:19 +0700

    add comments for item classes

--- classes/Item/Item.hpp
+++ classes/Item/Item.hpp
@@ -4,25 +4,39 @@
 #include <iostream>
 #include <string>
 using namespace std;
-// Item.hpp
+
 class Item{
 protected:
+    // id of the item
     const int id;
+    // name of the item
     const string name;
 public:
-    const bool isTool;  // true if item is a tool
+    // true if item is a tool
+    const bool isTool;  
 
+    // constructor
     Item() : id(0), name("none"), isTool(false) {}
+
+    // user defined constructor
     Item(int _id, string _name, bool _isTool) : id(_id), name(_name), isTool(_isTool) {}
+    
+    // cctor
     Item(const Item& other) : id(other.id), name(other.name), isTool(other.isTool) {}
     
+    // get id of the item
     int getId() const { return id; }
+
+    // get name of the item
     string getName() const { return name; }
 
+    // use the item
     virtual void use() = 0;
 
+    // get type or name of the item (for crafting)
     virtual string getType() const { return name; }
 
+    // clone the item
     virtual Item *clone() const = 0;
 };
 
--- classes/Item/Tool.hpp
+++ classes/Item/Tool.hpp
@@ -5,16 +5,25 @@
 // Tool.hpp
 class Tool : public Item {
 protected:
+    // durability of the tool
     int durability;
 public:
+    // constructor
     Tool() : Item(), durability(-1) {}
+    // user defined constructor
     Tool(int _id, string _name) : Item(_id, _name, true), durability(10) {}
+    // cctor
     Tool(int _id, string _name, int durability) : Item(_id, _name, durability), durability(durability) {}
+    // cctor
     Tool(const Tool& other) : Item(other) { durability = other.durability; }
+    
+    // use the tool
     void use() { durability--; }; 
 
+    // get durability of the tool
     int getDurability() const { return durability; }
 
+    // clone the tool
     Tool *clone() const { 
         Tool *clonedTool = new Tool(*this);
         return clonedTool;
